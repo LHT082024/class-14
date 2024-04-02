d0e6ceeb01d0608c4ef1736b6b939a17601abf54
SapphireShy <lucielity@gmail.com>
2024-04-02 12:07:05 +0200

    finished

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 console.log ('hello');
 
+
+//hent
 const burgerIcon = document.querySelector("#burgerIcon");
 console.log (burgerIcon);
 
+
+//modifiser
 function burgerMenu() {
-    console.log("clicked");
+    
+    //hent
+    const nav = document.querySelector("#nav");
+    console.log(nav);
+
+    //modifiser
+    nav.classList.toggle("HeaderNav");
 }
 
-
-
+//send
 burgerIcon.addEventListener ("click", burgerMenu);
